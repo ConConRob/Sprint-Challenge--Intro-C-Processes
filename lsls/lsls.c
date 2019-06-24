cf1a6bc40374b7c39edf86570da183c7e1cdc9d4
conconrob <connortprobertson@gmail.com>
2019-06-24 10:49:18 +0100

    getting input and can get dir

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <dirent.h>
 
+#define MAX 30
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
   // Parse command line
+  char path[MAX];
+  printf("Enter a path: ");
+  fgets(path, MAX, stdin);
 
+  printf("%s\n", path);
   // Open directory
+  
+  struct d;
+  DIR *d;
+  struct dirent *dir;
 
-  // Repeatly read and print entries
-
+  d = opendir(".");
+  // // Repeatly read and print entries
+  if (d)
+  {
+    while ((dir = readdir(d)) != NULL)
+    {
+      printf("%s\n", dir->d_name);
+    }
+  }
   // Close directory
 
   return 0;

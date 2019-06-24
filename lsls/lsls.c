81e70c245d5c6ea0587b24b8b943a9a87dc4fb65
conconrob <connortprobertson@gmail.com>
2019-06-24 11:54:43 +0100

    printing ls

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <string.h>
+#include <stdlib.h>
 #define MAX 30
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  char *path = malloc(100);
+  char *c_path = argv[0];
+  int i_path = 0;
   // Parse command line
-  char path[MAX];
-  printf("Enter a path: ");
-  fgets(path, MAX, stdin);
+  for ( int i =1; i<argc ; i++){
+    char *word = argv[i];
+    // copy the word to path
+    int length = strlen(word);
+    for (int i = 0; i<length; i++){
+      *(path + i_path) = *(word + i);
+      i_path++;
+    }
+  }
+  // add the last char for a string
+  if (i_path == 0){
+    *path = '.';
+  }
+  *(path + i_path + 1 ) = *"\0";
 
-  printf("%s\n", path);
-  // Open directory
-  
+  // Open directory  
   struct d;
   DIR *d;
   struct dirent *dir;
-
-  d = opendir(".");
-  // // Repeatly read and print entries
+  d = opendir(path);
+  // // // Repeatly read and print entries
   if (d)
   {
     while ((dir = readdir(d)) != NULL)
@@ -29,6 +41,6 @@ int main(int argc, char **argv)
     }
   }
   // Close directory
-
+  closedir(d);
   return 0;
 }
